4a5aba755ead878e3da4129ee5b2b6e3923ced46
Marcos <lamberti.marcos049@gmail.com>
2022-01-11 20:46:47 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+import Form from "./components/pages/Form";
+import Listing from "./components/pages/Listing";
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+

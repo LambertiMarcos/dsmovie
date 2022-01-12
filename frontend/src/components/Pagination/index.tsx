b69e91fc005ce027b7734fd4407869e1db0cd45c
Marcos <lamberti.marcos049@gmail.com>
2022-01-11 23:38:45 -0300

    MovieCard

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,4 +1,5 @@
-import { ReactComponent as Arrow } from 'assets/img/arrow.svg';
+import { ReactComponent as Arrow } from '../../assets/img/arrow.svg';
+import './style.css';
 
 function Pagination () {
 
--- frontend/src/components/pages/Listing/index.tsx
+++ frontend/src/components/pages/Listing/index.tsx
@@ -1,9 +1,36 @@
-import Pagination from "components/Pagination";
+import MovieCards from "../../MovieCards";
+import Pagination from "../../Pagination";
+
 
 function Listing(){
 
     return (
+        <>
         <Pagination />
+
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCards />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCards />
+                </div>   
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCards />
+                </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCards />
+                </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCards />
+                </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCards />
+                </div>             
+            </div>
+        </div>
+        </>
     );
 }
 
